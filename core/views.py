7c0c2b7e168cb22042e69dac7d8e710c30864a8d
DuyHuy <ninhduyhuy2001@gmail.com>
2023-10-23 13:32:41 +0700

    signup_backend

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 
 def homepage(request):
@@ -6,7 +7,23 @@ def homepage(request):
     return render(request, 'core/homepage.html')
 
 def signup(request):
-    
+    if request.method == "POST":
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+        username = request.POST.get('username', '')
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+
+        User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        return redirect('/login/')
+
     return render(request, 'core/signup.html')
 
 def login(request):
